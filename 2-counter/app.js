9ed1e28075fd0f9557862692718077c5f7770021
jawadr15 <78582999+jawadr15@users.noreply.github.com>
2021-02-20 13:23:24 -0500

    Resolved Repository

--- 2-counter/app.js
+++ 2-counter/app.js
@@ -9,29 +9,40 @@ console.log(btns)
 // for (let i = 0; i < btns.length; i++) {
 //   // btn.addEventListener()
 // }
+function addMadLibs(){
+  const companyName = companies[Math.floor(Math.random() * companies.length)]
+  const firstLetter = companyName.charAt(0)
+  const companyString = "My company is a" + ((vowels.indexOf(firstLetter) > -1) ? "n " : " ") + companyName + " for " + animals[Math.floor(Math.random() * animals.length)] + "s!"
+  //outputDiv.innerHTML += companyString
+  return companyString
+}
 
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
     if (styles.contains("decrease")) {
-      count--;
+      //count--;
+      value.textContent = addMadLibs()
     } else if (styles.contains("increase")) {
-      count *= 2;
+      //count *= 2;
       // count = count * 2
+      value.textContent = addMadLibs()
     } else {
-      count = 0;
+      value.textContent = ""
+
+      //count = 0;
     }
 
-    if (count > 0) {
-      value.style.color = "green";
-    }
-    if (count < 0) {
-      value.style.color = "red";
-    }
-    if (count === 0) {
-      value.style.color = "#222";
-    }
-    value.textContent = count;
+    //if (count > 0) {
+    //  value.style.color = "green";
+    //}
+    //if (count < 0) {
+    //  value.style.color = "red";
+    //}
+    //if (count === 0) {
+    // value.style.color = "#222";
+    //}
+    //value.textContent = count;
   });
 });
